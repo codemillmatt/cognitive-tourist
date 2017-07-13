d8b44d34f0ce3d7fb61d5d40fde34e97a941df67
Matt Soucoup <msoucoup@codemilltech.com>
2017-07-12 19:49:30 -0500

    REAL basic iOS computer vision

---
+++ src/CogTourist.Core/LandmarkIdentity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+namespace CogTourist.Core
+{
+    public class LandmarkIdentity
+    {
+        public string Description { get; set; }
+
+        public Stream Photo { get; set; }         
+    }
+}
